08184f2e6f024ab3911fcdfdaf768f5c31dfb37e
AjithKumar <akjstyles003@gmail.com>
2024-03-15 11:56:42 +0530

    git init

--- ui_react/src/App.jsx
+++ ui_react/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/contact' element={<Contact/>}/>
+     <Route path='/login' element={<Login/>}/>
+     <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
---
+++ ui_react/src/layouts/WebLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function WebLayout() {
+  return (
+   <>
+   </>
+  )
+}
+
+export default WebLayout
